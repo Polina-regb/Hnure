80df703d39e670020a07631e0321b4e28d985c2c
Polina-regb <evtupol@gmail.com>
2025-11-11 01:22:37 +0200

    Add files via upload

---
+++ culculz.py
@@ -0,0 +1,17 @@
+a = float(input("Введіть перше число: "))
+b = float(input("Введіть друге число: "))
+oper = input("Введіть операцію (+, -, *, /): ")
+
+if oper == '+':
+    print(f"Результат: {a + b}")
+elif oper == '-':
+    print(f"Результат: {a - b}")
+elif oper == '*':
+    print(f"Результат: {a * b}")
+elif oper == '/':
+    if b != 0:
+        print(f"Результат: {a / b}")
+    else:
+        print("Помилка")
+else:
+    print("Помилка")
